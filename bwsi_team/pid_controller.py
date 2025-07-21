354d056c260dc1ad8e415ec988b5238cd141c11b
iyeravyukt <avyiyer0811@gmail.com>
2025-07-21 16:12:23 -0400

    first commit guys

---
+++ bwsi_team/pid_controller.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64
+from mavros_msgs.msg import ManualControl
+
+class PIDController:
+    def __init__(self, kp, ki, kd, setpoint, dt=0.1):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.setpoint = setpoint
+        self.dt = dt
+
+        self.previous_error = 0.0
+        self.integral = 0.0
+
+    def compute(self, current_depth):
+        error = self.setpoint - current_depth
+        self.integral += error * self.dt
+        derivative = (error - self.previous_error) / self.dt
+
+        output = (self.kp * error) + (self.ki * self.integral) + (self.kd * derivative)
+        self.previous_error = error
+
+        return output
+
+
