7844a4f2cf8e102847b0e0087c742c5b0f2e2723
Ayan Ahmad <ayan.k.ahmaad01@gmail.com>
2025-07-22 15:58:36 -0400

    have fun part 2: the real stuff begins now!

---
+++ bluerov2_testers/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'bluerov2_testers'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='blue',
+    maintainer_email='ayan.k.ahmaad01@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'heading_lock = bluerov2_testers.HeadingLock:main',
+        ],
+    },
+)
