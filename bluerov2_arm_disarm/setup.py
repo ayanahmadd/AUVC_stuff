cc1e5c93bf40e12e45ebead4418a19b61209b3b4
Ayan Ahmad <ayan.k.ahmaad01@gmail.com>
2025-07-22 15:58:36 -0400

    have fun part 2: the real stuff begins now!

---
+++ bluerov2_arm_disarm/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'bluerov2_arm_disarm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='blue',
+    maintainer_email='ayan.k.ahmaad01@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'arm_disarm = bluerov2_arm_disarm.arm_disarm:main',
+            'auv_movement = bluerov2_arm_disarm.auv_movement:main',
+            'dance_movement = bluerov2_arm_disarm.dance_movement:main',
+            'test_diagonal = bluerov2_arm_disarm.test_diagonal_movement:main',
+        ],
+    },
+)
---
+++ bluerov2_controllers/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'bluerov2_controllers'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='blue',
+    maintainer_email='ayan.k.ahmaad01@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pid_depth = bluerov2_controllers.PID_depth:main',
+            'pid_heading = bluerov2_controllers.PID_heading:main',
+            'pid_controller = bluerov2_controllers.PIDController:main',
+        ],
+    },
+)
