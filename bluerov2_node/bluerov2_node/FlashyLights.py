b90475b3f810c1180c8108fb38c1f7519e0cabe7
starz0112 <zguo011235@gmail.com>
2025-07-28 14:51:10 -0400

    added basic template for flashing lights

---
+++ bluerov2_node/bluerov2_node/FlashyLights.py
@@ -0,0 +1,36 @@
+import rclpy
+from rclpy.node import Node
+from mavros_msgs.msg import OverrideRCIn
+
+
+class FlashLights(Node):
+    def __init__(self):
+        super().__init__("flashy_light_node")
+
+        self.command_pub = self.create_publisher(
+            OverrideRCIn, "override_rc", 10
+        )
+        
+    def turn_lights_on(self, level):
+        """
+        Turn the lights on.
+
+        Args:
+            level (int): The level to turn the lights on to. 0 is off, 100 is full
+        """
+        self.get_logger().info(f"Turning lights on to level {level}")
+        commands = OverrideRCIn()
+        commands.channels = [OverrideRCIn.CHAN_NOCHANGE] * 10
+        commands.channels[8] = 1000 + level * 10
+        commands.channels[9] = 1000 + level * 10
+        self.command_pub.publish(commands)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = FlashLights()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
